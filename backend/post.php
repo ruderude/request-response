767ff62e16113a50fa1cc48166c3db809abd8460
ruderude <rude1979@gmail.com>
2022-02-17 23:16:17 +0900

    録画後

--- backend/post.php
+++ backend/post.php
@@ -1,5 +1,6 @@
 <?php
 
+// フロント・バックエンドでのバリデーションやnullチェックなどは端折っています。
 if(isset($_POST)) {
     header('Content-type: application/json; charset=utf-8');
     echo json_encode(['text' => $_POST['post_test']]);
